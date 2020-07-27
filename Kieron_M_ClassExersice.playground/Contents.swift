3eed01bbb8e00f38673d88903b7a2a954063fa76
KieronMoore <67976013+KieronMoore@users.noreply.github.com>
2020-07-27 14:37:36 -0500

    Add files via upload

---
+++ Kieron_M_ClassExersice.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class carFeatures {
+    
+    var features : [String] =  ["tinted windows", "red tire smoke" , "a 3.6-Liter V6 24-Valve VVT Engine" , "and the color of it will be matte black"]
+    
+}
+
+var car = carFeatures()
+
+let myCar = car.features
+
+
+print("I will buy a 2018 Dodge Srt Demon and it will come with \(myCar).")
